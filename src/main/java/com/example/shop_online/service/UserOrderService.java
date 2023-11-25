c35bd75e6e87a1d14394c5a8444a94aa327b2f91
wujiahao17 <315776852@qq.com>
2023-11-25 08:51:30 +0800

    用户菜单管理

--- src/main/java/com/example/shop_online/service/UserOrderService.java
+++ src/main/java/com/example/shop_online/service/UserOrderService.java
@@ -2,6 +2,7 @@ package com.example.shop_online.service;
 
 import com.example.shop_online.entity.UserOrder;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.example.shop_online.vo.UserOrderVO;
 
 /**
  * <p>
@@ -12,5 +13,6 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * @since 2023-11-09
  */
 public interface UserOrderService extends IService<UserOrder> {
-
+    //提交订单
+    Integer addGoodsOrder(UserOrderVO orderVO);
 }
